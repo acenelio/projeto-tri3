309eb0d86b73e12dc16261ebf9a81464700d1e63
Nelio Alves <acenelio@gmail.com>
2018-10-25 20:33:37 -0300

    Enemy spawn point

---
+++ Assets/Scripts/EnemySpawnPointCtrl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnPointCtrl : MonoBehaviour {
+
+	public GameObject prefab;
+	public float objSpeed = 2f;
+	public float waitTime = 5f;
+	public Vector2[] directions;
+
+
+	void Start () {
+		StartCoroutine(Spawn());
+	}
+
+
+	IEnumerator Spawn() {
+		while (true) {
+			for (int i=0; i<directions.Length; i++) {
+				GameObject obj = Instantiate(prefab, transform.position, Quaternion.identity);
+				obj.GetComponent<SkeletonCtrl>().direction = directions[i];
+				obj.GetComponent<SkeletonCtrl>().speed = objSpeed;
+			}
+			yield return new WaitForSeconds(waitTime);
+		}
+	}
+}
