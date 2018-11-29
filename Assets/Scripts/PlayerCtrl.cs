257f8baaab705e0362fe7a55bfdb9be7861a55c9
Nelio Alves <acenelio@gmail.com>
2018-11-29 12:35:51 -0200

    Animacao de tiro + bugfix referencia do audio

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -89,6 +89,8 @@ public class PlayerCtrl : MonoBehaviour {
 
 		shootTime = 0f;
 
+		anim.Play("girl-shoot");
+
 		if (sr.flipX) {
 			AudioManager.instance.PlayLaserSound(leftShoot.gameObject);
 			Instantiate(leftShootPrefab, leftShoot.position, Quaternion.identity);
